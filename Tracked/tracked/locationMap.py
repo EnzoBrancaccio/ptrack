d04f30a90ff9ee3d95eeb5eb41f969cea93f5d88
Vincenzo Brancaccio <vincenzo.brancaccio@uni-ulm.de>
2021-06-08 16:37:37 +0200

    locationMap methods in Python

--- Tracked/tracked/locationMap.py
+++ Tracked/tracked/locationMap.py
@@ -18,12 +18,34 @@ class LocationMap(object):
         '''
         self.locationMap = dict()
     
-    def addLocations(self):
-        pass
+    def addLocations(self, lmA, lmB, prefix):
+        for key, value in lmB.items():
+            if(len(prefix) == 0):
+                lmA[key] = value
+            else:
+                if(key == "."):
+                    lmA[prefix] = value
+                else:
+                    lmA[prefix + "/" + key] = value
+        return lmA
     
-    def removeLocations(self):
-        pass
+    def removeLocations(self, lmA, prefix):
+        lmCopy = dict()
+        for key, value in lmA.items():
+            keyStartsWithPrefix = key.rfind(prefix, 0)
+            if(not keyStartsWithPrefix == 0):
+                lmCopy[key] = value
+        return lmCopy
     
-    def locationSlice(self):
-        pass
+    def locationSlice(self, lmA, prefix):
+        lmCopy = dict()
+        for key, value in lmA.items():
+            keyStartsWithPrefix = key.rfind(prefix, 0)
+            if(keyStartsWithPrefix == 0):
+                newKey = key[len(prefix):]
+                if(newKey == ""):
+                    lmCopy["."] = value
+                else:
+                    lmCopy[newKey[1:]] = value
+        return lmCopy
     
