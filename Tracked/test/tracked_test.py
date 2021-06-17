94ec75bb1e85d735afcaf91f2e9a9218affd67f3
Vincenzo Brancaccio <vincenzo.brancaccio@uni-ulm.de>
2021-06-17 16:26:19 +0200

    TrackedWithLocation test succcessful. Reworked Tracked class a bit with additional constructors (one for location_map, one for rosslt message location_header)

--- Tracked/test/tracked_test.py
+++ Tracked/test/tracked_test.py
@@ -28,10 +28,10 @@ class Test(unittest.TestCase):
         self.trackedTest1 = Tracked(5)
         
         self.value = self.trackedTest1.value
-        self.locationMap = self.trackedTest1.locationMap
+        self.location_map = self.trackedTest1.location_map
         
         self.assertEqual(self.value, 5, "Value")
-        self.assertFalse(self.locationMap["."].isValid(), "emptyLocation")
+        self.assertFalse(self.location_map["."].isValid(), "emptyLocation")
 
     def testIntFromMsgCreation(self):
         self.msg = Int32Tracked
@@ -40,10 +40,10 @@ class Test(unittest.TestCase):
         self.trackedTest2 = Tracked(self.msg.data)
         
         self.value = self.trackedTest2.value
-        self.locationMap = self.trackedTest2.locationMap
+        self.location_map = self.trackedTest2.location_map
         
         self.assertEqual(self.value, 7, "Value rosslt_msgs")
-        self.assertFalse(self.locationMap["."].isValid(), "emptyLocation")
+        self.assertFalse(self.location_map["."].isValid(), "emptyLocation")
 
     def testLocation(self):
         self.testLocation1 = Location()
@@ -64,6 +64,23 @@ class Test(unittest.TestCase):
         
         self.assertEqual(self.testLocation3.source_node, "mynode", "source node")
         self.assertEqual(self.testLocation3.location_id, 42, "location_id")
+
+    def testTrackedWithLocation(self):
+        self.trackedLocation1 = Location("mynode", 42)
+        
+        self.assertTrue(self.trackedLocation1.isValid(), "filled tracked Location")
+        
+        self.trackedDouble = Tracked(2.0, self.trackedLocation1)
+        
+        self.trackedDoubleLocation = self.trackedDouble.location_map["."]
+        self.trackedDoubleLocationId = self.trackedDoubleLocation.location_id
+        self.assertEqual(self.trackedDoubleLocationId, 42, "tracked location id")
+        
+        self.newTrackedDouble = self.trackedDouble
+        
+        self.newTrackedDoubleLocation = self.newTrackedDouble.location_map["."]
+        self.newTrackedDoubleLocationId = self.newTrackedDoubleLocation.location_id
+        self.assertEqual(self.newTrackedDoubleLocationId, 42, "tracked location id")
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
--- Tracked/tracked/tracked.py
+++ Tracked/tracked/tracked.py
@@ -5,24 +5,40 @@ Created on 01.06.2021
 '''
 
 from .location import Location
+from rosslt_msgs.msg import LocationHeader
 
 class Tracked(object):
     '''
     classdocs
     '''
 
-    def __init__(self, value, locationMap = None, location = None):
+    def __init__(self, value, location = None):
         '''
          Constructor
         '''
         self.value = value
-        
-        if locationMap is None:
-            self.locationMap = dict([(".", Location())])
-        else:
-            self.locationMap = locationMap
             
         if location is None:
             self.location = Location()
         else:
             self.location = location
+        
+        self.location_map = dict([(".", self.location)])
+        
+    @classmethod
+    def withLocationMap(cls, value, location_map):
+        newTracked = cls.__new__(cls)
+        super(Tracked, newTracked).__init__()
+        cls.value = value
+        cls.location_map = location_map
+        return newTracked
+    
+    @classmethod
+    def withRossltMsg(cls, value, rosslt_msg):
+        newTracked = cls.__new__(cls)
+        super(Tracked, newTracked).__init__()
+        cls.value = value
+        if(isinstance(rosslt_msg, LocationHeader)):
+            for i in range(len(rosslt_msg.paths)):
+                cls.location_map[rosslt_msg.paths[i]] = rosslt_msg.locations[i] 
+        return newTracked
