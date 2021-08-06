30b0acf29075c9ee6b297b85e1ef0ad74a571888
Vincenzo Brancaccio <vincenzo.brancaccio@uni-ulm.de>
2021-08-06 15:09:36 +0200

    added rclpy to project.

---
+++ Tracked/tracked/trackingnode.py
@@ -0,0 +1,19 @@
+'''
+Created on 06.08.2021
+
+@author: Enzo Brancaccio
+'''
+
+from rclpy.node import Node
+
+class TrackingNode(Node):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
