e865d953f266d8484218f187b78d352c6e546680
Vincenzo Brancaccio <vincenzo.brancaccio@uni-ulm.de>
2021-08-09 16:58:17 +0200

    Added a bit to LocationManager.

--- Tracked/tracked/locationManager.py
+++ Tracked/tracked/locationManager.py
@@ -19,6 +19,27 @@ class LocationManager(object):
         '''
         Constructor
         '''
-        self.sc_sub = node.create_subscription(SourceChange, "/sc") # callback?
+        self.source_locations = dict()
+        self.locations = list()
+        self.get_value_service = None
+        self.node = Node
+        
+        self.sc_sub = node.create_subscription(SourceChange, "/sc", self.on_source_change(self), 10)
         self.sc_pub = node.create_publisher(SourceChange, "/sc", 10)
-        self.get_value_service = node.create_service(GetValue, node.get_name() + "/get_slt_value")
+        self.get_value_service = node.create_service(GetValue, node.get_name() + "/get_slt_value", self.on_get_value(self, self), 10)
+        
+    def on_get_value(self, request, response):
+        if(len(self.locations) > request.location_id >= 0):
+            response.set_current_value(self.locations[request.location_id].get(request.location_id))
+            response.set_valid_id(True)
+        else:
+            response.set_valid_id(False)
+    
+    def on_source_change(self, msg):
+        if(msg.source_node == self.node.get_name()):
+            self.id = msg.location_id
+            if(0 > self.id >= len(self.locations)):
+                #RCLCPP_WARN(node.get_logger(), "source change to unknown location id %d", id);
+                pass
+            else:
+                self.locations[self.id].set(self.id, msg.new_value)
